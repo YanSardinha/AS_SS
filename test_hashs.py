d2210d0b7670903fe867ea7d5e7d63aa1b9562a2
Yan Sardinha <yansardinha1001@gmail.com>
2023-12-07 10:55:09 -0300

    fix: :bug: Erro de usuário UNIQUE corrigido ao executar os testes
    Foi corrigido o erro de usuário UNIQUE ao efetuar os testes dentro da função test_hashs.py. Foi criada a função usuario_existe que é utilizado pela função de inserir_usuarios_com_mesma_senha para evitar a tentativa de um cadastro de um usuário que já existe.

--- test_hashs.py
+++ test_hashs.py
@@ -1,23 +1,29 @@
 import bcrypt
 from utils import conectar_bd, gerar_salt_aleatorio
 
+def usuario_existe(cursor, username):
+    cursor.execute('SELECT COUNT(*) FROM usuarios WHERE username = ?', (username,))
+    return cursor.fetchone()[0] > 0
 
 def inserir_usuarios_com_mesma_senha(conn, cursor, senha, quantidade=5):
     for i in range(quantidade):
-        salt = gerar_salt_aleatorio()
-        senha_salt = senha + salt
-        senha_hash = bcrypt.hashpw(senha_salt.encode(), bcrypt.gensalt())
         username = f"usuario{i}"
 
-        cursor.execute('INSERT INTO usuarios (username, password, salt) VALUES (?, ?, ?)', (username, senha_hash, salt))
+        if not usuario_existe(cursor, username):
+            salt = gerar_salt_aleatorio()
+            senha_salt = senha + salt
+            senha_hash = bcrypt.hashpw(senha_salt.encode(), bcrypt.gensalt())
+
+            cursor.execute('INSERT INTO usuarios (username, password, salt) VALUES (?, ?, ?)', (username, senha_hash, salt))
+            print(f"Usuário '{username}' cadastrado com sucesso.")
+        else:
+            print(f"Usuário '{username}' já existe. Pulando para o próximo...")
 
     conn.commit()
-
 
 def recuperar_hashes_e_salts(cursor):
     cursor.execute('SELECT username, password, salt FROM usuarios')
     return cursor.fetchall()
-
 
 def verificar_senhas_iguais_com_hashes_diferentes(hashes_salts, senha):
     for username, hash, salt in hashes_salts:
@@ -27,7 +33,6 @@ def verificar_senhas_iguais_com_hashes_diferentes(hashes_salts, senha):
         else:
             print(f"Usuário {username} não tem a senha {senha}.")
 
-
 def main():
     conn, cursor = conectar_bd()
 
@@ -36,8 +41,8 @@ def main():
 
     hashes_salts = recuperar_hashes_e_salts(cursor)
     verificar_senhas_iguais_com_hashes_diferentes(hashes_salts, senha_teste)
-    conn.close()
 
+    conn.close()
 
 if __name__ == "__main__":
     main()
